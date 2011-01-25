744a354bd4ded523128cd4457179af885c6d26fb
Isao Yagi <isao.yagi@gmail.com>
2011-01-24 19:45:54 -0800

    symlink dot-*, ssh, svn, php-* configs

--- install-dot.sh
+++ install-dot.sh
@@ -1,19 +1,34 @@
 #!/bin/sh -e
 
-if [[ $PWD != $HOME ]]
-then
-	echo "this script should be run from $HOME"
-	exit
-fi
+#config file dir
+dotdir=~/.dotfiles
 
+err()
 {
-	dotdir=`dirname $0`
-	cd $dotdir
-	echo '- symlink dot-* files to ~'
-	for i in dot-*
-	do
-		echo ln -svi $dotdir/$i ${i/dot-/.}
-	done
-
-	
+	echo "$1 exiting"
+	exit
 }
+
+symlink_dotstar()
+{
+	echo '- symlink dot-* files to ~'
+	{
+		#cd here to so file list from dot-* glob doesn't have leading path parts
+		cd $dotdir
+		for i in dot-*
+		do
+			#replace dot- with . in filenames
+			ln -svi $dotdir/$i ~/${i/dot-/.}
+			#for cmd: ln -svi .dotfiles/dot-gitconfig /Users/isao/.gitconfig
+			#link is: ~/.gitconfig -> .dotfiles/dot-gitconfig
+		done
+	}
+}
+
+[[ ! -d $dotdir ]] && "install files should be at '$dotdir' but they're not"
+
+symlink_dotstar
+ln -svi $dotdir/dotssh-config ~/.ssh/config
+ln -svi $dotdir/dotsvn-config ~/.subversion/config
+ln -svi $dotdir/php-* \
+	`php -i |grep 'this dir for additional .ini files' |awk '{print $NF}'`
