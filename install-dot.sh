cbf14095e4f065b332165f667f32b65ac0f67a36
Isao Yagi <isao.yagi@gmail.com>
2011-06-23 17:37:24 -0700

    make more self-configuring, rm non-dotfile type stuff

--- install-dot.sh
+++ install-dot.sh
@@ -1,45 +1,24 @@
-#!/bin/sh -e
+#!/bin/bash -e
 
-#config file dir
-dotdir=~/Repos/dotfiles
+#glob to match files to symlink from this dir to $HOME
+dotglob='dot-*'
 
-err()
-{
-	echo "$1 exiting"
-	exit
-}
+#full path to this file's directory; i.e. /Users/isao/Repos/1st/dotfiles
+dotpath=`(cd $(dirname $0) && pwd)`
 
-symlink_dotstar()
-{
-	echo '- symlink dot-* files to ~'
-	{
-		#cd here to so file list from dot-* glob doesn't have leading path parts
-		cd $dotdir
-		for i in dot-*
-		do
-			#replace "dot-" with "." in filenames
-			# -i prompt if target file already exists
-			ln -svi $dotdir/$i ~/${i/dot-/.}
-			#for cmd: ln -svi .dotfiles/dot-gitconfig /Users/isao/.gitconfig
-			#link is: ~/.gitconfig -> .dotfiles/dot-gitconfig
-		done
-	}
-}
+#get filenames to symlink (without path parts)
+filelist=`(cd $dotpath && ls $dotglob)`
 
-[[ ! -d $dotdir ]] && err "source files should be at '$dotdir' but they're not"
+#replace home dir path with .; i.e. ./Repos/1st/dotfiles
+relpath=${dotpath/$HOME/.}
 
-symlink_dotstar
-ln -svi $dotdir/dotssh-config ~/.ssh/config
-ln -svi $dotdir/dotsvn-config ~/.subversion/config
-ln -svi $dotdir/php-* \
-	`php -i |grep 'this dir for additional .ini files' |awk '{print $NF}'`
-
-
-echo "paste these lines at the end of httpd.conf, or copy/symlink the files where they will"
-echo "be included:"
-echo
-for i in $dotdir/httpd-*
+#the cd and relpath stuff is to get nice short symlink references
+cd $HOME
+for i in $filelist
 do
-	echo "Include $i"
+  ln -svi $relpath/$i ${i/dot-/.}
 done
-echo
+
+#one-offs
+[[ -d .ssh ]] && ln -svi $relpath/dotssh-config .ssh/config
+[[ -d .subversion ]] && ln -svi $relpath/dotsvn-config .subversion/config
