339f9343a5f94f7818e23a402a03489a198cf586
Isao Yagi <isao.yagi@gmail.com>
2011-07-22 11:08:44 -0700

    - symlink full path to hook - param for repos to hook - pwd

--- git/install_post_commit_hooks.sh
+++ git/install_post_commit_hooks.sh
@@ -1,8 +1,15 @@
 #!/bin/sh -e
 
-file_to_hook=$(dirname $0)/post-commit
+[[ -z $@ ]] && {
+  echo "missing git repo path(s)" >&2
+  exit 9
+}
 
-for i in `find . -type d -path '*/.git/hooks' -maxdepth 2`
+#get full path to this dir
+file_to_hook=`(cd $(dirname $0) && pwd)`/post-commit
+
+#find hook dirs to symlink to post-commit from
+for i in `find $@ -type d -path '*/.git/hooks' -maxdepth 2`
 do
-  (cd $i && ln -svfi $file_to_hook)
+  (cd $i && pwd && ln -svfi $file_to_hook)
 done
