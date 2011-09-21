32066b36160d68649aad0af01900bd14ab45a0f9
Isao Yagi <isao.yagi@gmail.com>
2011-09-21 09:15:15 -0700

    okay, revert my nice relative path logic since it breaks when making a new `screen` when pwd isn't ~; comments; $() instead of ``

--- install-dot.sh
+++ install-dot.sh
@@ -3,21 +3,18 @@
 #glob to match files to symlink from this dir to $HOME
 dotglob='dot-*'
 
-#full path to this file's directory; i.e. /Users/isao/Repos/1st/dotfiles
-dotpath=`(cd $(dirname $0) && pwd)`
+#full path to this file's directory; i.e. /Users/isao/Repos/dotfiles
+dotpath=$(cd $(dirname $0) && pwd)
 
 #get list of dotfiles
-filelist=`(cd $dotpath && ls $dotglob)`
-
-#get relative path from ~ to this file's directory to make short symlinks
-relpath=${dotpath/$HOME/.}
+filelist=$(cd $dotpath && ls $dotglob)
 
 cd $HOME
 for i in $filelist
 do
   #replace "dot-" with "." for target of symlink; prompt before overwriting
-  #i.e. ln -svi ./Repos/dotfiles/dot-gitignore .gitignore
-  ln -svfi $relpath/$i ${i/dot-/.}
+  #i.e. ln -svi /Users/isao/Repos/dotfiles/dot-gitignore .gitignore
+  ln -svfi $dotpath/$i ${i/dot-/.}
 done
 
 #one-offs; n.b. adding an extra dot to $relpath here
