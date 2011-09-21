34ee8be70e3974d07b1fdc44fd33c6ae0eac3fd5
Isao Yagi <isao.yagi@gmail.com>
2011-09-21 10:48:41 -0700

    rename var, fix one-offs

--- install-dot.sh
+++ install-dot.sh
@@ -4,19 +4,19 @@
 dotglob='dot-*'
 
 #full path to this file's directory; i.e. /Users/isao/Repos/dotfiles
-dotpath=$(cd $(dirname $0) && pwd)
+abspath=$(cd $(dirname $0) && pwd)
 
 #get list of dotfiles
-filelist=$(cd $dotpath && ls $dotglob)
+filelist=$(cd $abspath && ls $dotglob)
 
 cd $HOME
 for i in $filelist
 do
   #replace "dot-" with "." for target of symlink; prompt before overwriting
   #i.e. ln -svi /Users/isao/Repos/dotfiles/dot-gitignore .gitignore
-  ln -svfi $dotpath/$i ${i/dot-/.}
+  ln -svfi $abspath/$i ${i/dot-/.}
 done
 
-#one-offs; n.b. adding an extra dot to $relpath here
-[[ -d .ssh ]] && ln -svfi .$relpath/dotssh-config .ssh/config
-[[ -d .subversion ]] && ln -svfi .$relpath/dotsvn-config .subversion/config
+#one-offs
+[[ -d .ssh ]] && ln -svfi $abspath/dotssh-config .ssh/config
+[[ -d .subversion ]] && ln -svfi $abspath/dotsvn-config .subversion/config
