af431759aa4fab30624c5250073690d677c23e74
Isao Yagi <isao.yagi@gmail.com>
2012-03-16 13:42:02 -0700

    make cwd default

--- git/install-hooks.sh
+++ git/install-hooks.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -e
 
-[[ -z $@ ]] && {
-  echo "missing git repo path(s). no hooks installed." >&2
-  exit 9
-}
+#git repo dirs
+destdirs=${@:-.}
+
+#echo $destdirs;exit
 
 #get full path to this dir
 hookdir=`cd $(dirname $0) && pwd`
@@ -12,7 +12,7 @@ hookdir=`cd $(dirname $0) && pwd`
 hookfiles="post-commit"
 
 #find hook dirs to symlink to post-commit from
-for i in `find $@ -type d -path '*/.git/hooks' -maxdepth 2`
+for i in `find $destdirs -type d -path '*/.git/hooks' -maxdepth 2`
 do
   (
     cd $i && pwd
