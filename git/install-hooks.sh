f453841e72c3b1756a9f97f4cc61ee417235d976
Isao Yagi <isao.yagi@gmail.com>
2012-03-26 10:00:57 -0700

    use cwd by default

--- git/install-hooks.sh
+++ git/install-hooks.sh
@@ -1,16 +1,16 @@
 #!/bin/sh -e
 
-#git repo dirs
-destdirs=${@:-.}
+#find git dirs from command line arguments; use cwd by default
+gitdirs=${@:-.}
 
-#get full path to this dir
+#get full path to this script's dir
 hookdir=`cd $(dirname $0) && pwd`
 
 #space delimited list of hook scripts relative to $hookdir
 hookfiles="post-commit"
 
 #find hook dirs to symlink to post-commit from
-for i in `find $destdirs -type d -path '*/.git/hooks' -maxdepth 2`
+for i in `find $gitdirs -type d -path '*/.git/hooks' -maxdepth 2`
 do
   (
     cd $i && pwd
