3bc6a9caefccc2d079cb98f01f2150f1ff30bd94
Isao Yagi <isao.yagi@gmail.com>
2012-11-19 17:21:03 -0800

    add jshint pre-commit hook

--- git/install-hooks.sh
+++ git/install-hooks.sh
@@ -7,7 +7,7 @@ gitdirs=${@:-.}
 hookdir=`cd $(dirname $0) && pwd`
 
 #space delimited list of hook scripts relative to $hookdir
-hookfiles='post-commit'
+hookfiles='pre-commit post-commit'
 
 #find hook dirs to symlink to post-commit from
 for i in `find $gitdirs -type d -path '*/.git/hooks' -maxdepth 2`
