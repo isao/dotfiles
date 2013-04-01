184db42d2e393c5408ab6c928c2292f411204704
Isao Yagi <isao@yahoo-inc.com>
2013-04-01 10:35:15 -0700

    minor, quote vars in case pathnames contain spaces

--- install-dot.sh
+++ install-dot.sh
@@ -4,19 +4,19 @@
 # renaming them with a leading dot instead of "dot-"
 
 #full path to this file's directory; i.e. /Users/isao/Repos/dotfiles
-abspath=$(cd $(dirname $0) && pwd)
+abspath=$(cd "$(dirname $0)" && pwd)
 
 #get list of dotfiles
-filelist=$(cd $abspath && ls 'dot-*')
+filelist=$(cd "$abspath" && ls 'dot-*')
 
 cd $HOME
 for i in $filelist
 do
   #replace "dot-" with "." for target of symlink; prompt before overwriting
   #i.e. ln -svi /Users/isao/Repos/dotfiles/dot-gitignore .gitignore
-  ln -svfi $abspath/$i ${i/dot-/.}
+  ln -svfi "$abspath/$i" ${i/dot-/.}
 done
 
 #one-offs
-[[ -d .ssh ]] && ln -svfi $abspath/dotssh-config .ssh/config
-[[ -d .subversion ]] && ln -svfi $abspath/dotsvn-config .subversion/config
+[[ -d .ssh ]] && ln -svfi "$abspath/dotssh-config .ssh/config"
+[[ -d .subversion ]] && ln -svfi "$abspath/dotsvn-config .subversion/config"
