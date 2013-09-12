f1a3570565c8f414d4853683f59fa1b2f9da8fb9
Isao Yagi <isao@yahoo-inc.com>
2013-09-11 18:03:39 -0700

    tweak install script; rm .ssh/config

--- install-dot.sh
+++ install-dot.sh
@@ -2,6 +2,7 @@
 
 # this script symlinks all dot files here to $HOME
 # renaming them with a leading dot instead of "dot-"
+# (run from anywhere, $CWD is not important)
 
 #full path to this file's directory; i.e. /Users/isao/Repos/dotfiles
 abspath=$(cd "$(dirname $0)" && pwd)
@@ -9,14 +10,16 @@ abspath=$(cd "$(dirname $0)" && pwd)
 #get list of dotfiles
 filelist=$(cd "$abspath" && ls dot-*)
 
+# -s symbolic, -v verbose, -f force, -i prompt before replacing anything
+link="ln -svfi"
+
 cd $HOME
 for i in $filelist
 do
   #replace "dot-" with "." for target of symlink; prompt before overwriting
   #i.e. ln -svi /Users/isao/Repos/dotfiles/dot-gitignore .gitignore
-  ln -svfi "$abspath"/$i ${i/dot-/.}
+  $link "$abspath"/$i ${i/dot-/.}
 done
 
 #one-offs
-[[ -d .ssh ]] && ln -svfi "$abspath"/dotssh-config .ssh/config
-[[ -d .subversion ]] && ln -svfi "$abspath"/dotsvn-config .subversion/config
+[[ -d .subversion ]] && $link "$abspath"/dotsvn-config .subversion/config
