09bc0d275997e6c68e8dee085427a08903104ac3
Isao Yagi <isao.yagi@gmail.com>
2014-07-01 10:36:58 -0700

    whsp

--- install-dot.sh
+++ install-dot.sh
@@ -4,10 +4,10 @@
 # renaming them with a leading dot instead of "dot-"
 # (run from anywhere, $CWD is not important)
 
-#full path to this file's directory; i.e. /Users/isao/Repos/dotfiles
+# full path to this file's directory; i.e. /Users/isao/Repos/dotfiles
 abspath=$(cd "$(dirname $0)" && pwd)
 
-#get list of dotfiles
+# get list of dotfiles
 filelist=$(cd "$abspath" && ls dot-*)
 
 # -s symbolic, -v verbose, -f force, -i prompt before replacing anything
@@ -16,10 +16,10 @@ link="ln -svfi"
 cd $HOME
 for i in $filelist
 do
-  #replace "dot-" with "." for target of symlink; prompt before overwriting
-  #i.e. ln -svi /Users/isao/Repos/dotfiles/dot-gitignore .gitignore
+  # replace "dot-" with "." for target of symlink; prompt before overwriting
+  # i.e. ln -svi /Users/isao/Repos/dotfiles/dot-gitignore .gitignore
   $link "$abspath"/$i ${i/dot-/.}
 done
 
-#one-offs
+# one-offs
 [[ -d .subversion ]] && $link "$abspath"/dotsvn-config .subversion/config
