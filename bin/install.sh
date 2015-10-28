64486ad05ef5d4f38f4eb775de2f2186aa8da0c2
Isao Yagi <isao.yagi@gmail.com>
2015-10-28 14:09:14 -0700

    Symlink ./bin*.sh to /usr/local/bin and remove the ".sh"

--- bin/install.sh
+++ bin/install.sh
@@ -1,23 +1,31 @@
 #!/bin/bash -e
 
-# this script symlinks all dot files here to $HOME adding a leading dot.
+# Usage: ~/repos/dotfile/bin/install.sh
 # (run from anywhere, $CWD is not important).
 
-# full path to this file's directory; i.e. /Users/isao/Repos/dotfiles
+# full path to this file's directory; i.e. /Users/isao/repos/dotfiles
 thisdir=$(dirname "$0")
 abspath=$(cd "$thisdir" && git rev-parse --show-toplevel)
-
-# get list of dotfiles
-filelist=$(/bin/ls -1 "$abspath")
 
 # -s symbolic, -v verbose, -i prompt before replacing anything
 link="ln -svi"
 
+# Symlink to all files in ./ from $HOME, adding a leading dot.
 cd "$HOME"
-for i in $filelist
+for i in $(/bin/ls -1 "$abspath")
 do
   if [[ -f "$abspath/$i" ]]
   then
     $link "$abspath/$i" ".$i"
+  fi
+done
+
+# Symlink to all files in ./bin from /usr/local/bin, removing the ".sh"
+cd /usr/local/bin
+for i in $(/bin/ls -1 "$abspath"/bin/*.sh)
+do
+  if [[ $(basename "$0") != $(basename "$i") ]]
+  then
+    $link "$i" $(basename "$i" .sh)
   fi
 done
