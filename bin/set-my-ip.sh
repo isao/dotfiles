a94f2403d08b292c3996fb2c63927c9b260ec98d
Isao Yagi <isao.yagi@gmail.com>
2016-03-07 14:56:27 -0800

    Update on change only, make shellcheck happy

--- bin/set-my-ip.sh
+++ bin/set-my-ip.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -eu
+#!/bin/bash -eu
 
 # config
 #
@@ -16,13 +16,13 @@ get_interface()
 
 get_ip()
 {
-    ifconfig -r $1 | awk '/inet /{print $2}'
+    ifconfig -r "$1" | awk '/inet /{print $2}'
 }
 
 
 # main
 #
-ip=$(get_ip $(get_interface))
+ip=$(get_ip "$(get_interface)")
 record_id=199535263
 ## To get $record_id -- rec_load_all to get DNS Record ID
 ## https://www.cloudflare.com/docs/client-api.html#s3.3
@@ -32,18 +32,25 @@ record_id=199535263
 #   -d email=$email \
 #   -d z=$domain
 
+oldip=$(host "$subdomain.$domain" | awk '{ print $4 }')
+[[ $oldip = "$ip" ]] && {
+    echo "* ip for $subdomain.$domain unchanged: $oldip"
+    exit 0
+}
+
+
 # Set the ip for A record dev for target domain $domain
 # https://www.cloudflare.com/docs/client-api.html#s5.2
 echo "* Setting DNS A record for $subdomain.$domain to $ip"
 curl -v https://www.cloudflare.com/api_json.html \
   -d a=rec_edit \
-  -d tkn=$token \
-  -d email=$email \
-  -d id=$record_id \
-  -d z=$domain \
-  -d name=$subdomain \
+  -d tkn="$token" \
+  -d email="$email" \
+  -d id="$record_id" \
+  -d z="$domain" \
+  -d name="$subdomain" \
   -d type=A \
-  -d content=$ip \
+  -d content="$ip" \
   -d service_mode=1 \
   -d ttl=120
 
