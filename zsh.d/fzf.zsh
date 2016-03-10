3826155073866427726c95ee823c6acf9a395447
Isao Yagi <isao.yagi@gmail.com>
2016-03-09 17:42:16 -0800

    Don't use stock fzf.zsh. Roll my own ^t fzf-file-widget.
    Stock ^t function used find, which is too noisy for work repos.

--- zsh.d/fzf.zsh
+++ zsh.d/fzf.zsh
@@ -3,7 +3,7 @@ then
     exit 0
 fi
 
-source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+#   mine from tcsh.d/fzf.tcsh
 
 # from within fzf:
 # ctrl-c copy the selected item
@@ -19,6 +19,57 @@ fi
 # fzf recent items
 alias fzf-recent='mdfind -onlyin ~/work -onlyin ~/Desktop -onlyin ~/Dropbox -onlyin ~/repos "date:this month" | fzf'
 
+# ctrl-f invoke fzf from the shell line editor
+# zle     -N   fzf-file-widget
+# bindkey '^f' fzf-file-widget
+
+
+
+
+
+# source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+
+# CTRL-T - Paste the selected file path(s) into the command line
+__fsel() {
+    ag -g "" --files-with-matches --all-text | fzf -m | while read item
+    do
+      [[ -n $item ]] && echo -n "${(q)item} "
+    done
+}
+
+fzf-file-widget() {
+    LBUFFER="${LBUFFER}$(__fsel)"
+    zle redisplay
+}
+
+zle     -N   fzf-file-widget
+bindkey '^T' fzf-file-widget
+
+#
+# # ALT-C - cd into the selected directory
+# fzf-cd-widget() {
+#   local cmd="${FZF_ALT_C_COMMAND:-"command find -L . \\( -path '*/\\.*' -o -fstype 'dev' -o -fstype 'proc' \\) -prune \
+#     -o -type d -print 2> /dev/null | sed 1d | cut -b3-"}"
+#   cd "${$(eval "$cmd" | $(__fzfcmd) +m):-.}"
+#   zle reset-prompt
+# }
+# zle     -N    fzf-cd-widget
+# bindkey '\ec' fzf-cd-widget
+#
+# # CTRL-R - Paste the selected command from history into the command line
+# fzf-history-widget() {
+#   local selected num
+#   selected=( $(fc -l 1 | fzf +s --tac +m -n2..,.. --tiebreak=index --toggle-sort=ctrl-r -q "${LBUFFER//$/\\$}") )
+#   if [ -n "$selected" ]; then
+#     num=$selected[1]
+#     if [ -n "$num" ]; then
+#       zle vi-fetch-history -n $num
+#     fi
+#   fi
+#   zle redisplay
+# }
+# zle     -N   fzf-history-widget
+# bindkey '^R' fzf-history-widget
 
 
 
