fd260bece5e744a647fffd25e52833cab730e770
Isao Yagi <isao.yagi@gmail.com>
2016-03-15 11:54:18 -0700

    make zsh functions from old tcsh aliases

--- zsh.d/functions.zsh
+++ zsh.d/functions.zsh
@@ -2,7 +2,31 @@ zman() {
   PAGER="less -g -s '+/^       "$1"'" man zshall
 }
 
+dict() {
+	curl -s "dict://dict.org/d:$1" | egrep -v '^[0-9]{3} .*|^\.'
+}
+
+word() {
+	grep "$1" /usr/share/dict/words
+}
+
+man2txt() {
+	man $* | col -b
+}
+
+manx() {
+	open "x-man-page://$1"
+}
+
+# Mac OSX
+rmmacmeta() {
+	find ${@:-.} \( -name '.DS_Store' -or -name '._*' \) -exec rm -v '{}' \;
+}
 
 ql() {
 	(qlmanage -p "$1" 2&>1 > /dev/null) &
+}
+
+bbproj() {
+	open "~/Dropbox/Documents/bbproj/$1"
 }
