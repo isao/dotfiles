2e85b5fd367abb16769c91e59d4f39f35ab684dc
Isao Yagi <isao.yagi@gmail.com>
2016-03-18 15:24:09 -0700

    rm scripts superceeded by zsh functions

--- bin/fcr.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -eu
-# fbr - checkout git branch (including remote branches)
-# https://github.com/junegunn/fzf/wiki/examples
-
-branches=$(git branch --all | grep -v HEAD) &&
-branch=$(echo "$branches" |
-       fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
-git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
