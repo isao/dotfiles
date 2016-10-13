65300bf553cfe6c20c9189f344f037c34cd0dbd6
Isao Yagi <isao.michael.yagi@ericsson.com>
2016-10-13 11:30:59 -0700

    fzf various
    - ^F^R find recent files with fzf
    - ^G^G find git modified files
    - replace ag with rg
    - compdef gnu fzf rg

--- zsh.d/fzf.zsh
+++ zsh.d/fzf.zsh
@@ -1,59 +1,84 @@
-type fzf ag >/dev/null || return
+type fzf rg >/dev/null || return
+type fzf >/dev/null && compdef _gnu_generic fzf
+type rg >/dev/null && compdef _gnu_generic rg
 
+
+# fzf configuration
+#
+export FZF_DEFAULT_OPTS='-0 --bind "ctrl-c:execute(echo {}|pbcopy),ctrl-o:execute(open {}),ctrl-l:execute(less {})" --color=light --expect=ctrl-c,ctrl-o,ctrl-l --extended-exact --no-sort --reverse'
 # from within fzf:
 # ctrl-c copy the selected item
 # ctrl-o open the selected item
 # ctrl-l open the selected item in less
-export FZF_DEFAULT_OPTS='-0 --bind "ctrl-c:execute(echo {}|pbcopy),ctrl-o:execute(open {}),ctrl-l:execute(less {})" --color=light --expect=ctrl-c,ctrl-o,ctrl-l --extended-exact --no-sort --reverse'
+export FZF_DEFAULT_COMMAND='rg "" --files-with-matches'
 
-if type ag >/dev/null
-then
-    export FZF_DEFAULT_COMMAND='ag -g "" --files-with-matches'
-fi
 
-# fzf recent items
-alias fzf-recent='mdfind -onlyin ~/work -onlyin ~/Desktop -onlyin ~/Dropbox -onlyin ~/repos "date:this month" | fzf'
-
+# CTRL-F
+# Select a file.
 #
-# CTRL-F - Insert file path(s) into the command line
 fzf-file-widget() {
-    LBUFFER="${LBUFFER}$(echo $(ag -g "" --files-with-matches --all-text | fzf -m | sed 1d))"
+    LBUFFER="${LBUFFER}$(echo $(rg "" --files-with-matches | fzf -m | sed 1d))"
     zle redisplay
 }
 zle -N fzf-file-widget
 bindkey '^F' fzf-file-widget
 
+# CTRL-F CTRL-R
+# Select a recent file or path via Spotlight/mdfind.
+#
+fzf-recentfile-widget() {
+	filelist=$(mdfind -onlyin ~/work -onlyin ~/Desktop -onlyin ~/Dropbox -onlyin ~/repos 'date:this month' | fzf -m | sed 1d)
+    LBUFFER="${LBUFFER}$(echo $filelist)"
+    zle redisplay
+}
+zle -N fzf-recentfile-widget
+bindkey '^F^R' fzf-recentfile-widget
+
+# fzf recent items
+# alias fzf-recent='mdfind -onlyin ~/work -onlyin ~/Desktop -onlyin ~/Dropbox -onlyin ~/repos "date:this month" | fzf'
 
 __fzf_gitbranches() {
     git for-each-ref --format='%(refname:short)' $1 | grep -v old/ | fzf | sed 1d
 }
 
-# CTRL-B CTRL-B - Insert (local) git branch into the command line
+# CTRL-B CTRL-B
+# Select a local git branch.
 #
-fzf-branches-widget() {
+fzf-gitbranches-widget() {
     LBUFFER="${LBUFFER}$(echo $(__fzf_gitbranches refs/heads/))"
     zle redisplay
 }
-zle -N fzf-branches-widget
-bindkey '^B^B' fzf-branches-widget
+zle -N fzf-gitbranches-widget
+bindkey '^B^B' fzf-gitbranches-widget
 
+# CTRL-B CTRL-B CTRL-B
+# Select any git branch.
 #
-# CTRL-B CTRL-B CTRL-B - Insert (any) git branch into command line
-fzf-allbranches-widget() {
+fzf-gitallbranches-widget() {
     LBUFFER="${LBUFFER}$(echo $(__fzf_gitbranches))"
     zle redisplay
 }
-zle -N fzf-allbranches-widget
-bindkey '^B^B^B' fzf-allbranches-widget
+zle -N fzf-gitallbranches-widget
+bindkey '^B^B^B' fzf-gitallbranches-widget
 
 gco() {
     branch="$(__fzf_gitbranches refs/heads/)"
-	[[ -n $branch ]] && git checkout $branch
+    [[ -n $branch ]] && git checkout $branch
 }
 
-
+# CTRL-G CTRL-G
+# Select a modified file.
 #
-# CTRL-R - Insert history item the command line
+fzf-gitmodified-widget() {
+    LBUFFER="${LBUFFER}$(echo $(git status --short | awk '{ print $2 }' | fzf -m | sed 1d))"
+    zle redisplay
+}
+zle -N fzf-gitmodified-widget
+bindkey '^G^G' fzf-gitmodified-widget
+
+# CTRL-R
+# Navigate history.
+#
 fzf-history-widget() {
     local selected num
     selected=( $(fc -l 1 | fzf +s --tac +m -n2..,.. --tiebreak=index --toggle-sort=ctrl-r -q "${LBUFFER//$/\\$}") )
