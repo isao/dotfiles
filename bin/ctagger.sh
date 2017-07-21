f16b1114560c404cc471eed4c5523de610ab5d93
Isao Yagi <isao.michael.yagi@ericsson.com>
2017-07-20 17:41:07 -0700

    slightly more general ctagger for *.ts

--- bin/ctagger.sh
+++ bin/ctagger.sh
@@ -21,7 +21,7 @@ alert()
 
 #   CHECKS
 #
-type ctags || err 1 "Error: you need to install ctags (brew install universal-ctags)."
+type ctags >/dev/null || err 1 "Error: you need to install ctags (brew install universal-ctags)."
 [[ -f ~/.ctags ]] || err 3 "Error: you need to install ~/.ctags https://git.io/vDE8C"
 
 #   PARAMS
@@ -47,6 +47,9 @@ then
     paths='Web/scripts Web/typescript Web/scripts/OPEN_SOURCE_SOFTWARE/typescriptDefinitions Web/node_modules/typescript/lib/lib.*.d.ts'
     language='--languages=typescript'
     excludes="$excludes --exclude=OPEN_SOURCE_SOFTWARE --exclude=napa"
+elif [[ -d 'node_modules/typescript/lib' ]]
+then
+    paths='node_modules/typescript/lib/lib.*.d.ts'
 fi
 
 # Use git repo's base directory if applicable.
