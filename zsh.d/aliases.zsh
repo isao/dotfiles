3f8771c3d0b123c21980bd9d08d71733251fce23
Isao Yagi <isao.yagi@gmail.com>
2018-02-26 15:16:30 -0800

    lsd, llsd
    Improved ls for just dirs. Hat tip to @UnixToolTip

--- zsh.d/aliases.zsh
+++ zsh.d/aliases.zsh
@@ -6,10 +6,12 @@ alias cd..='cd ..'
 alias ls='ls -FG'
 alias ll='/bin/ls -lG'
 alias kk=ll
-alias lsd='find . -type d -depth 1 | xargs ls -dl "{}" \;'  # ll dirs only
 alias ls.='ls -dG .?*'  # ls dot files & dirs only
 alias ll.='ls -dGl .?*' # ls dot files & dirs only
 alias la='ll -A'
+#alias lsd='find . -type d -depth 1 | xargs ls -dl "{}" \;'  # ll dirs only
+alias lsd='/bin/ls -d */'   # ls just directories
+alias llsd='/bin/ls -dl */' # ll just directories
 
 #
 # one-liners
