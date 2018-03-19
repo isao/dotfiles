1fdec58b90db9f662c663bda8c0573610ccbaca4
Isao Yagi <isao.yagi@gmail.com>
2018-03-19 13:27:48 -0700

    ctagger
    - mov special case handling to external file .ctagger  at git base dir
    - add Less.ctags
    - update TypeScript.ctags
    - update exclude.ctags

--- bin/ctagger.sh
+++ bin/ctagger.sh
@@ -1,17 +1,9 @@
 #!/bin/bash -eo pipefail
 
-#   FUNCTIONS
-#
 err()
 {
     echo $2 >&2
     exit $1
-}
-
-maketags()
-{
-    cd "$basedir"
-    ctags $excludes $args $paths
 }
 
 alert()
@@ -19,36 +11,27 @@ alert()
     osascript -e "display notification \"$basedir\" with title \"$scriptname\""
 }
 
-#   CHECKS
+#
+#   Are ctags configured?
 #
 type ctags >/dev/null || err 1 "Error: you need to install ctags (brew install universal-ctags)."
 [[ -d ~/.ctags.d ]] || err 3 "Error: you need to configure ~/.ctags.d"
 
-#   PARAMS
+#
+#   Environment.
 #
 scriptname=$(basename "$0" .sh)
-args=$*
-excludes=
-paths=
-basedir=
-
 # If invoked from BBEdit Script Menu...
 [[ -n $BB_DOC_PATH ]] && cd "$(dirname "$BB_DOC_PATH")"
-
-# If invoked via .git/hooks...
-[[ -n $GIT_DIR ]] && args=
-
-# If we're in the ReachClient repo...
-if [[ $(git remote -v 2>/dev/null) =~ /MRGIT/_git/ReachClient ]]
-then
-    paths='Web/scripts Web/typescript Web/scripts/OPEN_SOURCE_SOFTWARE/typescriptDefinitions Web/node_modules/typescript/lib/lib.*.d.ts'
-    excludes="--exclude=OPEN_SOURCE_SOFTWARE --exclude=napa"
-fi
-
-# Use git repo's base directory if applicable.
 basedir="$(git rev-parse --show-toplevel 2>/dev/null || pwd)"
+cd "$basedir"
 
-
-#   MAIN
 #
-(maketags && alert) &
+#   Load arguments to ctags from a file, one per line, ignoring lines with "#".
+#
+conf=$(test -r .ctagger && grep -v \# .ctagger | xargs echo)
+
+#
+#   Invoke ctags in the background, raise notification when done.
+#
+(ctags $conf && alert) &
---
+++ bin/foo.ts
@@ -0,0 +1,6 @@
+
+function foo(s: string): void {
+    console.log(s);
+}
+
+foo('hey-o');
