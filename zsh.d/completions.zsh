bead45736c54807e85735d25199dd11dae727413
Isao Yagi <isao.yagi@gmail.com>
2018-04-10 16:00:36 -0700

    completion minor
    use-cache doesn't do anything?

--- zsh.d/completions.zsh
+++ zsh.d/completions.zsh
@@ -9,7 +9,13 @@ compinit
 # When completing from the middle of a word, move the cursor to the end of the word
 # setopt always_to_end
 
+# On an ambiguous completion, instead of listing possibilities or beeping, insert
+# the first match immediately.  Then when completion is requested again, remove
+# the first match and insert the second match, etc.  When there are no more
+# matches, go back to the first one again.  reverse-menu-complete may be used  to
+# loop through the list in the other direction. This option overrides AUTO_MENU.
 setopt menu_complete
+
 # setopt list_packed
 
 # https://github.com/zanshin/dotfiles/blob/master/zsh/setopt.zsh
@@ -21,11 +27,20 @@ setopt complete_in_word # Allow completion from within a word/phrase
 
 setopt numeric_glob_sort
 
+# This way you tell zsh comp to take the first part of the path to be
+# exact, and to avoid partial globs. Now path completions became nearly
+# immediate.
+zstyle ':completion:*' accept-exact '*(N)'
+zstyle ':completion:*' use-cache on
+zstyle ':completion:*' cache-path ~/.zsh_completion_cache
+
 # Allow lower-case characters to match upper case ones.
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # zsh-lovers: cd will never select the parent directory (e.g.: cd ../<TAB>):
 zstyle ':completion:*:cd:*' ignore-parents parent pwd
+# remove slash if argument is a directory
+zstyle ':completion:*' squeeze-slashes true
 
 type shellcheck >/dev/null && \
     compdef _gnu_generic shellcheck
@@ -40,20 +55,11 @@ then
     ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets)
 fi
 
-#
+
 # gulp-autocompletion-zsh
 # https://github.com/robbyrussell/oh-my-zsh/blob/master/plugins/gulp/gulp.plugin.zsh
 # Autocompletion for your gulp.js tasks
-#
-# Copyright(c) 2014 André König <andre.koenig@posteo.de>
-# MIT Licensed
-# André König
-# Github: https://github.com/akoenig
-# Twitter: https://twitter.com/caiifr
-#
-# Grabs all available tasks from the `gulpfile.js`
-# in the current directory.
-#
+# Copyright(c) 2014 André König <andre.koenig@posteo.de> MIT Licensed André König
 function $$gulp_completion {
     compls=$(gulp --tasks-simple 2>/dev/null)
     completions=(${=compls})
