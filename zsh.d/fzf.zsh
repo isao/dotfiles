65d379d18fb4ae0220436e177ac4eaec40701e94
Isao Yagi <isao.yagi@gmail.com>
2018-06-13 11:35:09 -0700

    fzf: various chrun
    - rename functions
    - change bound keys back to ^g^g etc
    - revert \er to just insert selected recent dir, instead of doing a `cd <dir>`
    - filter out /dev/Web/ and /skinned/ from recent file list.

--- zsh.d/fzf.zsh
+++ zsh.d/fzf.zsh
@@ -51,39 +51,35 @@ fzf-redraw-prompt() {
 }
 zle -N fzf-redraw-prompt
 
-
-# Select recent dirs, and ones tagged "wip".
-fzf-dirs-widget() {
-    local dir="$({cat ~/.zdirs; mdfind tag:wip AND kind:folders} | awk '!x[$0]++' | fzf --no-multi)"
+recent-dirs() {
+    {   
+        [[ -r  ~/.zdirs ]] && cat  ~/.zdirs
+        mdfind tag:wip AND kind:folders
+    } | awk '!x[$0]++'
     # ^ Dedupe without changing the order (to respect recency) -> awk '!x[$0]++'
     # https://stackoverflow.com/a/11532197/8947435
     # $0 holds the entire contents of a line and square brackets are array
     # access. So, for each line of the file, the node of the array x is
     # incremented and the line printed if the content of that node was not (!)
     # previously set.
-
-    # See /usr/local/Cellar/fzf/0.17.3/shell/key-bindings.zsh
-    if [[ -z "$dir" ]]; then
-      zle redisplay
-      return 0
-    fi
-
-    cd "$dir"
-    local ret=$?
-    zle fzf-redraw-prompt
-    typeset -f zle-line-init >/dev/null && zle zle-line-init
-    return $ret
 }
-zle     -N    fzf-dirs-widget
-bindkey '^f^d' fzf-dirs-widget               # Change to a recent (or "wip" tagged) directory.
 
-# Select a recent file or path via Spotlight/mdfind.
-fzf-recentfile-widget() {
-    LBUFFER+="$(mdfind -onlyin ~/work -onlyin ~/Desktop -onlyin ~/Dropbox -onlyin ~/repos 'kMDItemFSContentChangeDate >= $time.today(-9)' | fzf -m | xargs)"
+# Select a directory from dirstack ( ~/.zdirs) or things tagged "wip" w/spotlight.
+# See /usr/local/Cellar/fzf/0.17.3/shell/key-bindings.zsh, which does `cd`.
+fzf-dirs-widget() {
+    LBUFFER+="$(recent-dirs | fzf --no-multi)"
     zle redisplay
 }
-zle -N fzf-recentfile-widget
-bindkey '^f^r' fzf-recentfile-widget        # Pick a recent file.
+zle     -N    fzf-dirs-widget
+bindkey '\er' fzf-dirs-widget
+
+# Select a recent file or path via Spotlight/mdfind.
+fzf-recent-files-widget() {
+    LBUFFER+="$(mdfind -onlyin ~/work -onlyin ~/Desktop -onlyin ~/Dropbox -onlyin ~/repos 'kMDItemFSContentChangeDate >= $time.today(-9)' | fzf -m -q '!/dev/Web/ !/repos/skinned/' | xargs)"
+    zle redisplay
+}
+zle -N fzf-recent-files-widget
+bindkey '\err' fzf-recent-files-widget # Pick a recent file.
 
 # From /usr/local/opt/fzf/shell/key-bindings.zsh replace "$(__fzfcmd)" with "fzf"
 # CTRL-R - Paste the selected command from history into the command line
@@ -92,7 +88,7 @@ fzf-history-widget() {
   local selected num
   setopt localoptions noglobsubst noposixbuiltins pipefail 2> /dev/null
   selected=( $(fc -rl 1 |
-    FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS -n2..,.. --tiebreak=index --bind=ctrl-r:toggle-sort $FZF_CTRL_R_OPTS --query=${(qqq)LBUFFER} +m" fzf) )
+    FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS -n2..,.. --tiebreak=index --bind=ctrl-r:toggle-sort --query=${(qqq)LBUFFER} +m" fzf) )
   local ret=$?
   if [ -n "$selected" ]; then
     num=$selected[1]
@@ -104,8 +100,8 @@ fzf-history-widget() {
   typeset -f zle-line-init >/dev/null && zle zle-line-init
   return $ret
 }
-zle     -N   fzf-history-widget
-bindkey '^r' fzf-history-widget             # CTRL-R: Replay a history entry.
+zle -N fzf-history-widget
+bindkey '^r' fzf-history-widget # Replay a history entry.
 
 
 #
@@ -113,14 +109,14 @@ bindkey '^r' fzf-history-widget             # CTRL-R: Replay a history entry.
 #
 
 # Select git modified file(s).
-fzf-gitmodified-widget() {
+fzf-git-modified-widget() {
     LBUFFER+="$(git status --short | fzf --exit-0 --preview 'git diff --color {2}'| awk '{print $2}' | xargs)"
     zle redisplay
 }
-zle -N fzf-gitmodified-widget
-bindkey '^g^g' fzf-gitmodified-widget       # ESC GG: Pick a new or modified file from git status.
+zle -N fzf-git-modified-widget
+bindkey '^g^g' fzf-git-modified-widget # Pick changed file in git.
 
-__fzf_gitbranches() {
+__git-branches() {
     git for-each-ref --format='%(refname:short)' $1
 }
 
@@ -130,25 +126,25 @@ __fzf_preview_gitshow() {
 
 # Select a local git branch.
 fzf-gitbranches-widget() {
-    LBUFFER+="$(__fzf_gitbranches refs/heads/ | __fzf_preview_gitshow -q '!old/ ' )"
+    LBUFFER+="$(__git-branches refs/heads/ | __fzf_preview_gitshow -q '!old/ ' )"
     zle redisplay
 }
 zle -N fzf-gitbranches-widget
-bindkey '^g^b' fzf-gitbranches-widget       # ESC GB: pick a git local branch
+bindkey '^g^b' fzf-gitbranches-widget # Pick a git local branch.
 
 # Select any git branch.
-fzf-gitallbranches-widget() {
-    LBUFFER+="$(__fzf_gitbranches | __fzf_preview_gitshow -q '!origin/Dev/releases/ !/submit/request- !old/ ')"
+fzf-git-branches-all-widget() {
+    LBUFFER+="$(__git-branches | __fzf_preview_gitshow -q '!origin/Dev/releases/ !/submit/request- !old/ ')"
     zle redisplay
 }
-zle -N fzf-gitallbranches-widget
-bindkey '\^g^b^b' fzf-gitallbranches-widget
+zle -N fzf-git-branches-all-widget
+bindkey '^g^b^b' fzf-git-branches-all-widget # Pick any git branch.
 
 
 # Select a git stash
-fzf-gitstash-widget() {
+fzf-git-stash-widget() {
     LBUFFER+="$(git stash list | cut -d : -f 1 | fzf --preview 'git stash show --color -p {}' --preview-window='right:80%')"
     zle redisplay
 }
-zle -N fzf-gitstash-widget
-bindkey '^g^s' fzf-gitstash-widget
+zle -N fzf-git-stash-widget
+bindkey '^g^t' fzf-git-stash-widget # Pick a git stash.
