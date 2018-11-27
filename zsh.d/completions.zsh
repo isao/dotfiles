fe8fe41393da70b10519b2593f79469f2c3f1d1c
Isao Yagi <isao.yagi@gmail.com>
2018-11-26 17:02:08 -0800

    completion: comment out unsetopt menu_complete

--- zsh.d/completions.zsh
+++ zsh.d/completions.zsh
@@ -11,7 +11,7 @@ setopt list_packed
 # https://github.com/zanshin/dotfiles/blob/master/zsh/setopt.zsh
 setopt always_to_end # When completing from the middle of a word, move the cursor to the end of the word
 setopt auto_menu # show completion menu on successive tab press. needs unsetop menu_complete to work
-unsetopt menu_complete # do not autoselect the first completion entry
+#unsetopt menu_complete # do not autoselect the first completion entry
 setopt auto_name_dirs # any parameter that is set to the absolute name of a directory immediately becomes a name for that directory
 setopt complete_in_word # Allow completion from within a word/phrase
 
