c86ae7b4c91f2f7a3668281484a519ca9f9be66d
Isao Yagi <isao.yagi@gmail.com>
2020-03-02 16:05:49 -0800

    minor node updates

--- zsh.d/nodejs.zsh
+++ zsh.d/nodejs.zsh
@@ -1,9 +1,3 @@
-## nvm
-if [[ -e "$HOME/.zsh-nvm/zsh-nvm.plugin.zsh" ]]
-then
-    source "$HOME/.zsh-nvm/zsh-nvm.plugin.zsh"
-fi
-
 hash node npm >& /dev/null || return
 
 path=(node_modules/.bin $path)
@@ -18,16 +12,17 @@ compdef nn-complete nn
 
 # Functions
 
-link-node6() {
+link-node10() {
     brew unlink node
-    brew link --force node@6
+    brew link --force node@10
 }
 
 link-node() {
-    brew unlink node@6
+    brew unlink node@10
     brew link node
 }
 
+# TODO enable auto-upgrading of this code
 ###-begin-npm-completion-###
 #
 # npm command completion script
@@ -35,56 +30,53 @@ link-node() {
 # Installation: npm completion >> ~/.bashrc  (or ~/.zshrc)
 # Or, maybe: npm completion > /usr/local/etc/bash_completion.d/npm
 #
+_npm_completion () {
+local words cword
+if type _get_comp_words_by_ref &>/dev/null; then
+  _get_comp_words_by_ref -n = -n @ -n : -w words -i cword
+else
+  cword="$COMP_CWORD"
+  words=("${COMP_WORDS[@]}")
+fi
 
-if type complete &>/dev/null; then
-  _npm_completion () {
-    local words cword
-    if type _get_comp_words_by_ref &>/dev/null; then
-      _get_comp_words_by_ref -n = -n @ -n : -w words -i cword
-    else
-      cword="$COMP_CWORD"
-      words=("${COMP_WORDS[@]}")
-    fi
-
-    local si="$IFS"
-    IFS=$'\n' COMPREPLY=($(COMP_CWORD="$cword" \
-                           COMP_LINE="$COMP_LINE" \
-                           COMP_POINT="$COMP_POINT" \
-                           npm completion -- "${words[@]}" \
-                           2>/dev/null)) || return $?
-    IFS="$si"
-    if type __ltrim_colon_completions &>/dev/null; then
-      __ltrim_colon_completions "${words[cword]}"
-    fi
-  }
-  complete -o default -F _npm_completion npm
-elif type compdef &>/dev/null; then
-  _npm_completion() {
-    local si=$IFS
-    compadd -- $(COMP_CWORD=$((CURRENT-1)) \
-                 COMP_LINE=$BUFFER \
-                 COMP_POINT=0 \
-                 npm completion -- "${words[@]}" \
-                 2>/dev/null)
-    IFS=$si
-  }
-  compdef _npm_completion npm
-elif type compctl &>/dev/null; then
-  _npm_completion () {
-    local cword line point words si
-    read -Ac words
-    read -cn cword
-    let cword-=1
-    read -l line
-    read -ln point
-    si="$IFS"
-    IFS=$'\n' reply=($(COMP_CWORD="$cword" \
-                       COMP_LINE="$line" \
-                       COMP_POINT="$point" \
+local si="$IFS"
+IFS=$'\n' COMPREPLY=($(COMP_CWORD="$cword" \
+                       COMP_LINE="$COMP_LINE" \
+                       COMP_POINT="$COMP_POINT" \
                        npm completion -- "${words[@]}" \
                        2>/dev/null)) || return $?
-    IFS="$si"
-  }
-  compctl -K _npm_completion npm
+IFS="$si"
+if type __ltrim_colon_completions &>/dev/null; then
+  __ltrim_colon_completions "${words[cword]}"
 fi
+}
+complete -o default -F _npm_completion npm
+elif type compdef &>/dev/null; then
+_npm_completion() {
+local si=$IFS
+compadd -- $(COMP_CWORD=$((CURRENT-1)) \
+             COMP_LINE=$BUFFER \
+             COMP_POINT=0 \
+             npm completion -- "${words[@]}" \
+             2>/dev/null)
+IFS=$si
+}
+compdef _npm_completion npm
+elif type compctl &>/dev/null; then
+_npm_completion () {
+local cword line point words si
+read -Ac words
+read -cn cword
+let cword-=1
+read -l line
+read -ln point
+si="$IFS"
+IFS=$'\n' reply=($(COMP_CWORD="$cword" \
+                   COMP_LINE="$line" \
+                   COMP_POINT="$point" \
+                   npm completion -- "${words[@]}" \
+                   2>/dev/null)) || return $?
+IFS="$si"
+}
+compctl -K _npm_completion npm
 ###-end-npm-completion-###
