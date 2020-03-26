3da4a8570c56cb1c48c68c084bd8a1b9ca9dedc6
Isao Yagi <isao.yagi@gmail.com>
2020-03-26 13:07:12 -0700

    rename & tweak file & dir widgets
    also comment edits, comment out zsh-interactive-cd.plugin.zsh

--- zsh.d/fzf.zsh
+++ zsh.d/fzf.zsh
@@ -6,9 +6,9 @@ type fzf fd >/dev/null || return
 
 # Note: not using the functions that ship with fzf
 # https://github.com/junegunn/fzf#key-bindings-for-command-line
-#   source "/opt/brew/Cellar/fzf/0.21.0-1/shell/key-bindings.zsh"
+#   source "$HOMEBREW_PREFIX/Cellar/fzf/0.21.0-1/shell/key-bindings.zsh"
 # https://github.com/junegunn/fzf#fuzzy-completion-for-bash-and-zsh
-#   source "/opt/brew/Cellar/fzf/0.21.0-1/shell/completion.zsh"
+#   source "$HOMEBREW_PREFIX/Cellar/fzf/0.21.0-1/shell/completion.zsh"
 
 # fzf configuration
 # ctrl-c copy the selected item
@@ -26,16 +26,14 @@ export FZF_DEFAULT_OPTS='--color=light --tabstop=4 --cycle --exact --multi --rev
 # Find in file
 # Based on: https://github.com/junegunn/fzf/wiki/Examples#searching-file-contents
 fif() {
+    local rgflags preview
     if [[ -z $1 ]]
     then
         echo "Missing search string"
         return 1
     fi
-
-    local rgflags preview
     rgflags='--colors "match:bg:green" --ignore-case --pretty --context 5'
     preview="highlight -O ansi {} 2> /dev/null | rg $rgflags '$1' || rg $rgflags '$1' {}"
-
     rg --files-with-matches --no-messages "$1" . | fzf --preview $preview
 }
 
@@ -50,13 +48,19 @@ fin() {
 #
 #   Tip: Show all the custom `bindkey` assignments with alias `showkeys`.
 
-# Select file(s).
-fzf-file-widget() {
+fzf-filenames-widget() {
     LBUFFER+="$(fd -t f | fzf --preview 'echo {}; highlight {} --out-format xterm256 --quiet --force --style fine_blue' | xargs)"
     zle redisplay
 }
-zle -N fzf-file-widget
-bindkey '^f^f' fzf-file-widget              # Find file.
+zle -N fzf-filenames-widget
+bindkey '^f^f' fzf-filenames-widget             # Find file.
+
+fzf-dirnames-widget() {
+    LBUFFER+="$(fd -t d | fzf --preview 'echo {}; tree {}' | xargs)"
+    zle redisplay
+}
+zle -N fzf-dirnames-widget
+bindkey '^f^f^f' fzf-dirnames-widget            # Find dir.
 
 recent-dirs() {
     {
@@ -72,23 +76,23 @@ recent-dirs() {
 }
 
 # Select a directory from dirstack ( ~/.zdirs) or things tagged "wip" w/spotlight.
-# See /usr/local/Cellar/fzf/0.17.3/shell/key-bindings.zsh, which does `cd`.
-fzf-dirs-widget() {
+# See $HOMEBREW_PREFIX/Cellar/fzf/0.17.3/shell/key-bindings.zsh, which does `cd`.
+fzf-recent-dirs-widget() {
     LBUFFER+="$(recent-dirs | fzf --no-multi)"
     zle redisplay
 }
-zle     -N    fzf-dirs-widget
-bindkey '\er' fzf-dirs-widget
+zle     -N    fzf-recent-dirs-widget
+bindkey '\er' fzf-recent-dirs-widget
 
 # Select a recent file or path via Spotlight/mdfind.
 fzf-recent-files-widget() {
-    LBUFFER+="$(mdfind -onlyin ~/work -onlyin ~/Desktop -onlyin ~/Dropbox -onlyin ~/repos 'kMDItemFSContentChangeDate >= $time.today(-9)' | fzf -m -q '!/dev/Web/ !/repos/skinned/' | xargs)"
+    LBUFFER+="$(mdfind -onlyin ~/work -onlyin ~/Desktop -onlyin ~/notes -onlyin ~/work 'kMDItemFSContentChangeDate >= $time.today(-14)' | fzf -m | xargs)"
     zle redisplay
 }
 zle -N fzf-recent-files-widget
-bindkey '\err' fzf-recent-files-widget # Pick a recent file.
+bindkey '\erf' fzf-recent-files-widget # Pick a recent file.
 
-# From /usr/local/opt/fzf/shell/key-bindings.zsh replace "$(__fzfcmd)" with "fzf"
+# From $HOMEBREW_PREFIX/opt/fzf/shell/key-bindings.zsh replace "$(__fzfcmd)" with "fzf"
 # CTRL-R - Paste the selected command from history into the command line
 # Overrides default "^R" history-incremental-search-backward
 fzf-history-widget() {
@@ -111,5 +115,5 @@ zle -N fzf-history-widget
 bindkey '^r' fzf-history-widget # Replay a history entry.
 
 
-[[ -r "$dotfiles/zsh.d/zsh-interactive-cd.plugin.zsh" ]] &&\
-    source "$dotfiles/zsh.d/zsh-interactive-cd.plugin.zsh"
+# [[ -r "$dotfiles/zsh.d/zsh-interactive-cd.plugin.zsh" ]] &&\
+#     source "$dotfiles/zsh.d/zsh-interactive-cd.plugin.zsh"
