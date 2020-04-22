528750f435bea9e4e5334a15c2e0805971601936
Isao Yagi <isao.yagi@gmail.com>
2020-04-22 14:32:11 -0700

    ctagger: use -R if no config file is found

--- bin/ctagger.sh
+++ bin/ctagger.sh
@@ -12,24 +12,28 @@ function alert() {
 #
 #   Are ctags configured?
 #
-type ctags >/dev/null || err 1 "Error: you need to install ctags (brew install --HEAD universal-ctags/universal-ctags/universal-ctags)."
+type ctags >/dev/null \
+    || err 1 "Error: you need to install ctags (brew install --HEAD universal-ctags/universal-ctags/universal-ctags)."
+
 [[ -d ~/.ctags.d ]] || err 3 "Error: you need to configure ~/.ctags.d"
 
 #
 #   Environment.
 #
 scriptname=$(basename "$0" .sh)
+
 # If invoked from BBEdit Script Menu...
 [[ -n $BB_DOC_PATH ]] && cd "$(dirname "$BB_DOC_PATH")"
+
 basedir="$(git rev-parse --show-toplevel 2>/dev/null || pwd)"
-cd "$basedir"
 
 #
 #   Load arguments to ctags from a file, one per line, ignoring lines with "#".
 #
-conf=$(test -r .ctagger && grep -v \# .ctagger | xargs)
+conf=$(test -r .ctagger && grep -v \# .ctagger | xargs || echo '-R')
 
 #
 #   Invoke ctags in the background, raise notification when done.
 #
+cd "$basedir"
 ctags $@ $conf && alert
