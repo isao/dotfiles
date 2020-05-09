ca7c2137e49cbe474c83ddf3462d2e00761a882e
Isao Yagi <isao.yagi@gmail.com>
2020-05-09 09:59:16 -0700

    ctags: index hbs files

---
+++ bin/ctags-index-hbs.zsh
@@ -0,0 +1,34 @@
+#!/bin/zsh -e
+
+# Generate a ctags file for Ember Handlebars components, if run from the root of
+# and Ember project repo.
+
+whence ctags >/dev/null || {
+	echo 'ctags must be installed.'
+	exit
+}
+
+[[ -d app/components ]] || exit
+
+cat <<EOF > ./app/tags
+!_TAG_FILE_FORMAT   2   /extended format; --format=1 will not append ;" to lines/
+!_TAG_FILE_SORTED   0   /0=unsorted, 1=sorted, 2=foldcase/
+!_TAG_OUTPUT_FILESEP    slash   /slash or backslash/
+!_TAG_OUTPUT_MODE   u-ctags /u-ctags or e-ctags/
+!_TAG_PATTERN_LENGTH_LIMIT  96  /0 for no limit/
+!_TAG_PROGRAM_AUTHOR    Universal Ctags Team    //
+!_TAG_PROGRAM_NAME  Universal Ctags /Derived from Exuberant Ctags/
+!_TAG_PROGRAM_URL   https://ctags.io/   /official site/
+!_TAG_PROGRAM_VERSION   0.0.0   /3671ad7/
+EOF
+
+for pathname in app/components/**/*.hbs
+do
+    # The file name is the template "class name".
+    filename=$(basename $pathname .hbs)
+    nameparts=(${(s:-:)filename})
+    classname=${(j::)${(C)nameparts}}
+
+    # Create a "class" entry for the file, use line 1.
+    printf "%s\t$pathname\t1;\"c\tline:1\n" $classname >> ./app/tags
+done
