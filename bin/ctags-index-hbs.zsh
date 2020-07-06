c940f323afc3553df1aee9cd432f3f1c2164682c
Isao Yagi <isao.yagi@gmail.com>
2020-07-06 13:37:23 -0700

    minor: comments

--- bin/ctags-index-hbs.zsh
+++ bin/ctags-index-hbs.zsh
@@ -3,8 +3,6 @@
 # Generate a ctags file for Ember Handlebars components, if run from the root of
 # and Ember project repo.
 
-hbs_files=(app/components/**/*.hbs)
-hbs_ctags=./wta.hbs.tags
 
 whence ctags >/dev/null || {
 	echo 'ctags must be installed.'
@@ -12,6 +10,9 @@ whence ctags >/dev/null || {
 }
 
 [[ -d app/components ]] || exit
+
+hbs_files=(app/components/**/*.hbs)
+hbs_ctags=./wta.hbs.tags
 
 cat <<EOF > $hbs_ctags
 !_TAG_FILE_FORMAT   2   /extended format; --format=1 will not append ;" to lines/
@@ -29,7 +30,11 @@ for pathname in $hbs_files
 do
     # The file name is the template "class name".
     filename=$(basename $pathname .hbs)
+
+    # Split on "-".
     nameparts=(${(s:-:)filename})
+
+    # Uppercase initial letters of each part, and join.
     classname=${(j::)${(C)nameparts}}
 
     # Create a "class" entry for the file, use line 1.
