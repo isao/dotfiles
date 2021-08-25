3179a4182041da6b52ef831097bc956bf76a6a6e
Isao Yagi <isao.yagi@gmail.com>
2021-08-25 13:16:03 -0700

    nanoleaf.sh - lights on/off/info/broghtness

---
+++ bin/nanoleaf.sh
@@ -0,0 +1,53 @@
+#!/bin/bash -e
+
+# https://documenter.getpostman.com/view/1559645/RW1gEcCH#cbbfadae-fa4a-4cdf-933e-1e9d8fbf40d0
+
+# Get IP from router.
+IP="192.168.7.157"
+
+# On the Nanoleaf controller, hold the on-off button for 5-7 seconds until the
+# LED starts flashing in a pattern. Then:
+#    curl -X POST http:/$IP:16021/api/v1/new
+TOKEN="in8SPOGUk3Zj3iFfUCrFkKfK4wH6O3lz"
+
+state-on() {
+    curl --silent --location \
+        --request PUT "http:/$IP:16021/api/v1/$TOKEN/state" \
+        --header 'Content-Type: application/json' \
+        --data-raw "{\"on\": {\"value\": $1}}"
+}
+
+brightness() {
+    if [[ -z $1 ]]
+    then
+        curl --silent --location \
+            --request GET "http://$IP:16021/api/v1/$TOKEN/state/brightness"
+    else
+        curl --silent --location \
+            --request PUT "http://$IP:16021/api/v1/$TOKEN/state" \
+            --data-raw "{\"brightness\": {\"value\":$1}}"
+    fi
+}
+
+
+case $1 in
+    'on' )
+        state-on true
+        ;;
+
+    'off' )
+        state-on false
+        ;;
+
+    'brightness' )
+        brightness "$2"
+        ;;
+
+    'info' )
+        curl --silent --location --request GET "http://$IP:16021/api/v1/$TOKEN/"
+        ;;
+
+    * )
+        echo "Usage: $(basename "$0") on|off|info|brightness [0-100]" >&2
+        ;;
+esac
