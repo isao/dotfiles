1cfc80ec88b8b38ffd2e70835d778a387adbeea2
Isao Yagi <isao.yagi@gmail.com>
2022-07-21 14:39:09 -0700

    minor use long flag name in git aliases

--- zsh.d/git.zsh
+++ zsh.d/git.zsh
@@ -3,8 +3,8 @@
 #
 alias gd='git diff'
 alias gdt='git difftool'
-alias gf='git fetch -p --tags'
-alias gp='git pull -p --tags --ff-only'
+alias gf='git fetch --prune --tags'
+alias gp='git pull --prune --tags --ff-only'
 alias gu='git gui browser'
 alias gr='git remote -v'
 alias gsa='git stash apply'
