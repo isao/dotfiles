f6d37e615a4e6f7b062a04603e743f93327f4a20
Isao Yagi <isao.yagi@gmail.com>
2023-01-30 16:08:30 -0800

    volta path

--- zsh.d/nodejs.zsh
+++ zsh.d/nodejs.zsh
@@ -1,20 +1,24 @@
 # https://volta.sh
-#export VOLTA_HOME="$HOME/.volta";path=("$VOLTA_HOME/bin" $path)
-# if [[ -d "$HOME/.volta" ]]
-# then
-#     export VOLTA_HOME="$HOME/.volta"
-#     path=("$VOLTA_HOME/bin" $path)
-#     # source <(volta completions 'zsh')
-#     # ^- errors -> _arguments:comparguments:325: can only be called from completion function
-# fi
+# To upgrade volta:
+#   brew upgrade volta
+#   volta setup
+#
+# export VOLTA_HOME="$HOME/.volta";path=("$VOLTA_HOME/bin" $path)
+if [[ -d "$HOME/.volta" ]]
+then
+    export VOLTA_HOME="$HOME/.volta"
+    # path=("$VOLTA_HOME/bin" $path)
+    # ^- Already added to `/etc/paths`
+    # source <(volta completions 'zsh')
+    # ^- errors -> _arguments:comparguments:325: can only be called from completion function
+fi
 
 whence node npm >/dev/null || return
 
 source <(npm completion)
 
-#grep -q ./node_modules/.bin /etc/paths || path=(./node_modules/.bin $path)
+# grep -q ./node_modules/.bin /etc/paths || path=(./node_modules/.bin $path)
 
 alias nn='npm run'
 
 alias nnci='npm ci && never_index_artifacts'
-
