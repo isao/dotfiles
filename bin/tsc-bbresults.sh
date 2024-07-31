9399200364884abac371ae8df61fecd32ea6a8de
Isao Yagi <isao.yagi@gmail.com>
2024-07-31 11:03:39 -0700

    tsc-bbresults: fix pattern

--- bin/tsc-bbresults.sh
+++ bin/tsc-bbresults.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -eu
 
-pattern='^(?P<file>\w.+?)\((?P<line>\d+),(?P<col>\d+)\): (?P<type>\w+) (?P<msg>.+(?:\n +[^\n]+)*)'
-
-tsc --noEmit --pretty false $* | bbresults -p '$pattern'
+tsc --noEmit --pretty false "$@" \
+    | bbresults -p '^(?P<file>.+?)\((?P<line>\d+),(?P<col>\d+)\): (?P<type>\w+) (?P<msg>.+)'
