ee6a5b8eede725f31c7ad0dca81589574d3a0c96
Isao Yagi <isao.yagi@gmail.com>
2025-10-27 12:03:22 -0700

    disable zsh-interactive-cd

--- zsh.d/fzf.zsh
+++ zsh.d/fzf.zsh
@@ -115,5 +115,5 @@ zle -N fzf-history-widget
 bindkey '^r' fzf-history-widget # Replay a history entry.
 
 # https://github.com/changyuheng/zsh-interactive-cd
-[[ -r "$dotfiles/zsh.d/vendor/zsh-interactive-cd.plugin.zsh" ]] && \
-    source "$dotfiles/zsh.d/vendor/zsh-interactive-cd.plugin.zsh"
+# [[ -r "$dotfiles/zsh.d/vendor/zsh-interactive-cd.plugin.zsh" ]] && \
+#     source "$dotfiles/zsh.d/vendor/zsh-interactive-cd.plugin.zsh"
