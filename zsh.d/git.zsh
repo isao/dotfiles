c2bf712ebc054d044ec476bc73a40e39b414fb8f
Isao Yagi <isao.yagi@gmail.com>
2026-01-18 16:30:41 -0800

    fzf-git widget tweaks
    - use `--accept-nth=1` instead of `| awk '{ print $1 }'`, etc.
    - reorg, rename helper

--- zsh.d/git.zsh
+++ zsh.d/git.zsh
@@ -69,18 +69,9 @@ git-fetch-pr() {
     git fetch up "pull/$1/head:pr$1" && git switch "pr$1"
 }
 
-
 #
-#   FZF GIT WIDGETS
+#   GIT WIDGET HELPERS
 #
-
-# Select git modified file(s).
-fzf-git-modified-widget() {
-    LBUFFER+="$(git status --short | fzf -n2 --preview 'git diff --color {2}' --exit-0 | awk '{print $2}' | xargs)"
-    zle redisplay
-}
-zle -N fzf-git-modified-widget
-bindkey '^g^g' fzf-git-modified-widget # Pick changed file in git.
 
 __git-branches() {
     git for-each-ref --format='%(refname:short)' --sort -authordate $1
@@ -90,6 +81,33 @@ __fzf_preview_gitlog_branch() {
     fzf --multi --preview='echo "branch: {}"; git log -9 --color --decorate --format=fuller --stat {}' $@
 }
 
+# git log -> fzf with preview. Allows fuzzy finding by email, sha, or commit
+# subject. returns selected sha(s)
+__fzf-git-log() {
+    # git log format info:
+    # %C(___)   color
+    # %h        short commit sha
+    # %s        commit subject
+    # %al       author email local-part (the part before the @ sign)
+    git log --format='%C(auto)%h %s %C(blue)@%al' --color=always $@ \
+        | fzf --accept-nth=1 --ansi --no-sort --preview='git show --color --stat --decorate --pretty=fuller -p {1}' \
+        | xargs
+}
+
+
+#
+#   FZF GIT WIDGETS
+#
+
+# Select git modified file(s).
+fzf-git-modified-widget() {
+    LBUFFER+="$(git status --short | fzf -n2  --accept-nth=2 --exit-0 --preview 'git diff --color {2}' | xargs)"
+    zle redisplay
+}
+zle -N fzf-git-modified-widget
+bindkey '^g^g' fzf-git-modified-widget # Pick changed file in git.
+
+
 # Select a local git branch.
 fzf-gitbranches-widget() {
     LBUFFER+="$(__git-branches refs/heads/ | __fzf_preview_gitlog_branch  -q '!old/ ')"
@@ -98,6 +116,7 @@ fzf-gitbranches-widget() {
 zle -N fzf-gitbranches-widget
 bindkey '^g^b' fzf-gitbranches-widget # Pick a git local branch.
 
+
 # Select any git branch.
 fzf-git-branches-all-widget() {
     LBUFFER+="$(__git-branches | __fzf_preview_gitlog_branch)"
@@ -105,6 +124,7 @@ fzf-git-branches-all-widget() {
 }
 zle -N fzf-git-branches-all-widget
 bindkey '^g^b^b' fzf-git-branches-all-widget # Pick any git branch.
+
 
 # Select a git stash
 fzf-git-stash-widget() {
@@ -120,32 +140,20 @@ fzf-git-stash-widget() {
 zle -N fzf-git-stash-widget
 bindkey '^g^t' fzf-git-stash-widget # Pick a git stash.
 
-# git log -> fzf with preview. Allows fuzzy finding by email, sha, or commit
-# subject. returns selected sha(s)
-git-log-fzf() {
-    # git log format info:
-    # %C(___)   color
-    # %h        short commit sha
-    # %s        commit subject
-    # %al       author email local-part (the part before the @ sign)
-    git log --format='%C(auto)%h %s %C(blue)@%al' --color=always $@ \
-        | fzf --ansi --no-sort --preview='git show --color --stat --decorate --pretty=fuller -p {1}' \
-        | awk '{ print $1 }' \
-        | xargs
-}
 
 # Select a commit from any branch with fzf, allowing fuzzy matching on the
 # author email local-part.
 fzf-git-pick-commit() {
-    LBUFFER+="$(git-log-fzf --all --no-merges -9999)"
+    LBUFFER+="$(__fzf-git-log --all --no-merges -9999)"
     zle redisplay
 }
 zle -N fzf-git-pick-commit
 bindkey '^g^p' fzf-git-pick-commit # Pick a git commit from any branch to cherry-pick
 
+
 # Select a commit since the last merge to --fixup or --squash.
 fzf-git-fixup-commit() {
-    LBUFFER+="$(git-log-fzf $(git log --merges --format=%h -1)..HEAD)"
+    LBUFFER+="$(__fzf-git-log $(git log --merges --format=%h -1)..HEAD)"
     zle redisplay
 }
 zle -N fzf-git-fixup-commit
